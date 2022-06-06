c07c090406cd12d6dafb81d5d034de9daa8f210f
Bailey Tjiong <btjiong@outlook.com>
2022-06-06 18:55:37 +0200

    Add accuracy to the predictions

--- foreground.js
+++ foreground.js
@@ -1,16 +1,22 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log(request.message);
     // Get the Stack Overflow title element
     const parent = document
         .getElementById('question-header')
         .getElementsByTagName('h1')[0];
+
+    // Get the actual Stack Overflow tags
+    const items = document
+        .getElementsByClassName('d-flex ps-relative fw-wrap')[0]
+        .getElementsByTagName('a');
 
     // Create a italics element on a newline
     if (parent.getElementsByTagName('br').length == 0) {
         const newline = document.createElement('br');
         const element = document.createElement('i');
         const tags = document.createTextNode(
-            `[Predicted tags: ${request.message}]`
+            `[Predicted tags (${accuracy(request.message, items)}): ${
+                request.message
+            }]`
         );
 
         // Put the predicted tags inside the created element
@@ -19,3 +25,18 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         parent.appendChild(element);
     }
 });
+
+function accuracy(pred, actual) {
+    var tp = 0;
+    var fn = 0;
+    for (var i = 0; i < actual.length; i++) {
+        if (pred.includes(actual[i].textContent)) {
+            tp++;
+        } else {
+            fn++;
+        }
+    }
+    const fp = pred.length - tp;
+    const accuracy = tp / (tp + fn + fp);
+    return +parseFloat(accuracy).toFixed(2);
+}
