7e86adbb0231193fad9179b81d7e9d9325576300
Bailey Tjiong <btjiong@outlook.com>
2022-06-08 19:23:42 +0200

    Fix in url

--- background.js
+++ background.js
@@ -8,15 +8,14 @@ chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
     // Check if the page is loaded, otherwise the title is not correct
     if (change.status == 'complete') {
         // Check if the current tab is a Stack Overflow post
-        if (/^https:\/\/stackoverflow/.test(tab.url)) {
+        if (/^https:\/\/stackoverflow.com\/questions\/*\//.test(tab.url)) {
             chrome.tabs.executeScript(null, { file: './foreground.js' }, () => {
                 // Gets the tab title and removes ' - Stack Overflow'
                 const title = tab.title.slice(0, -17);
 
                 // Create a new POST request to predict the tags of the Stack Overflow title
                 const req = new XMLHttpRequest();
-                // const baseUrl = 'https://so-classifier.herokuapp.com/predict';
-                const baseUrl = 'http://127.0.0.1/predict';
+                const baseUrl = 'https://so-classifier.herokuapp.com/predict';
                 req.open('POST', baseUrl, true);
                 req.setRequestHeader(
                     'Content-type',
